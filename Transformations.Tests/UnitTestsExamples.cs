85610a09841cd8f84b427371ab6f9378c916bbb4
Jonathan F <psyjpf@nottingham.ac.uk>
2019-03-14 13:22:54 +0000

    Added Unit Test Example

---
+++ Transformations.Tests/UnitTestsExamples.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Transformations.Tests
+{
+    [TestFixture]
+    public class UnitTestExamples
+    {
+        [Test]
+        public void Example1()
+        {
+            Assert.IsFalse(false, "test");
+        }
+
+        //[Test]
+        //public void Example2()
+        //{
+        //    Assert.AreEqual(false, "");
+        //}
+    }
+}
